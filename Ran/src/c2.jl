dafc0c5b6ad49351e4fbbbc3642d8593ea9a53cf
Markus Gierlinger <markus@cast.ai>
2024-05-05 14:53:34 +0200

    add naive shell listener

--- Ran/src/c2.jl
+++ Ran/src/c2.jl
@@ -1,20 +1,34 @@
+using UUIDs
+using Sockets
 
+function handleNewImplant(conn)
+    id = uuid4()
+    println("New implant connected $id")
+
+    while !eof(conn)
+        msg = readline(conn)
+        println(" >>> Request [$id]: $msg")
+        write(conn, "ack $msg")
+    end
+
+    println("done with implant $id")
+end
 
 function onStartListener(ev::StartListener)
     @info "start listener on port $(ev.port)"
 
-    @async begin
-        HTTP.WebSocket.open("ws://0.0.0.0:$(ev.port)") do ws
-            for msg in ws
-                println("got msg via listener: $msg")
-            end
+    # HTTP.serve!(handleNewImplant, "0.0.0.0",ev.port; async=true)
+    errormonitor(@async begin
+        server = Sockets.listen(ev.port)
+        while true # TODO maybe create Event to stop  here and return the event as result event from this fn, so the event can be set somewhere else
+            sock = Sockets.accept(server)
+            @async handleNewImplant(sock)
         end
-    end
-
+    end)
     @warn "post listener"
 end
 
 function startC2(bus:: MessageBus)
-    @info "Starting C2"
+    println("starting C2")
     register!(bus, StartListener, onStartListener)
 end 
