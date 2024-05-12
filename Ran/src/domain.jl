c0970bd56e40d95a2697ee68d40523e9a0b9e08b
Markus Gierlinger <mataryki@gmail.com>
2024-05-12 16:47:11 +0200

    handle new simple session connection

--- Ran/src/domain.jl
+++ Ran/src/domain.jl
@@ -1,13 +1,29 @@
 abstract type Message end
 
-include("./commands.jl")
-include("./events.jl")
-
-
-
 abstract type Entity end
 
-struct System <: Entity 
-    id:: String
-    name::String
+abstract type AbstractRelation end
+Base.@kwdef struct  Relation <: AbstractRelation
+    name::AbstractString
+    source::AbstractString
+    destination::AbstractString
+    data::Union{AbstractString, Nothing} = nothing
 end
+
+Base.@kwdef struct System <: Entity 
+    id:: AbstractString
+    name::AbstractString
+    os::Union{AbstractString,Nothing} = nothing
+end
+
+# abstract type SessionType end
+# struct SimpleSession <: SessionType end
+# struct ImplantSession <: SessionType end
+@enum SessionType begin
+    SimpleSession
+    ImplantSession
+end
+
+
+include("./commands.jl")
+include("./events.jl")
