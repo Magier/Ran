2faa7adc042004b541f8dc5f7ff6a719c8b8c823
Markus Gierlinger <mataryki@gmail.com>
2024-10-06 21:39:26 +0200

    add new facts event

--- src/domain/event.go
+++ src/domain/event.go
@@ -51,6 +51,19 @@ func (e ErrorMsg) UiMessage() string {
 	return e.Msg
 }
 
+type NewFacts struct {
+	Entities  []Entity
+	Relations []Relation
+	Assets    []Asset
+}
+
+func (e NewFacts) MessageName() string {
+	return "NewFacts"
+}
+func (e NewFacts) String() string {
+	return fmt.Sprintf("Received new facts: %d entities, %d relatiosn, %d assets", len(e.Entities), len(e.Relations), len(e.Assets))
+}
+
 type EnvVarsExtracted struct {
 	Source Entity
 	Vars   map[string]string
