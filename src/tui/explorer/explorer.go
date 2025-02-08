86e35041d8f91a7e63d1b0090039f5ecf1b0191b
Markus Gierlinger <mataryki@gmail.com>
2025-02-08 21:52:14 +0100

    traverse knowledge graph in TUI explorer, instead of building custom tree

--- src/tui/explorer/explorer.go
+++ src/tui/explorer/explorer.go
@@ -1,7 +1,7 @@
 package explorer
 
 import (
-	"sort"
+	"fmt"
 	"strings"
 
 	"github.com/Magier/Ran/campaign"
@@ -12,29 +12,38 @@ import (
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	tree "github.com/charmbracelet/lipgloss/tree"
 )
 
 type entry struct {
-	text string
-	ref  *Node
+	level       int
+	label       string
+	entity      domain.Entity
+	isExpanded  bool
+	isPwnd      bool
+	accessLevel domain.AccessLevel
+	numChildren int
 }
 
-func (e entry) String() string {
-	return e.text
+func (e entry) Label() string {
+	indent := strings.Repeat(" ", e.level*2)
+	icon := getIcon(e.entity.GetKind())
+	label := fmt.Sprintf("%s %s", icon, e.entity.GetName())
+	if !e.isExpanded && e.numChildren > 0 {
+		label += fmt.Sprintf(" (%d)", e.numChildren)
+	}
+	return indent + label
 }
 
 // Rework to ordered map
 type Model struct {
-	campaign     *campaign.Campaign
-	entitiesTree *Node
-	tree         *tree.Tree
-	entries      []entry
-	focused      bool
-	width        float32
-	style        lipgloss.Style
-	cursor       int
-	viewport     viewport.Model
+	campaign *campaign.Campaign
+	entries  []entry
+	entities []entry
+	focused  bool
+	width    float32
+	style    lipgloss.Style
+	cursor   int
+	viewport viewport.Model
 }
 
 func NewExplorer(c *campaign.Campaign, width float32) Model {
@@ -43,7 +52,7 @@ func NewExplorer(c *campaign.Campaign, width float32) Model {
 		// Foreground(lipgloss.Color("#7D56F4")).
 		// Background(lipgloss.Color("#FAFAFA")).
 		PaddingLeft(1).
-		PaddingTop(1).
+		PaddingTop(0).
 		// Height(35).
 		Width(22).
 		BorderStyle(lipgloss.RoundedBorder()).
@@ -54,29 +63,17 @@ func NewExplorer(c *campaign.Campaign, width float32) Model {
 		BorderBottom(false)
 
 	return Model{
-		campaign:     c,
-		entitiesTree: newTree(),
-		tree:         tree.New(),
-		focused:      false,
-		width:        width,
-		style:        style,
-		cursor:       -1,
+		campaign: c,
+		focused:  false,
+		width:    width,
+		style:    style,
+		cursor:   -1,
 	}
 }
 
 func (m Model) Init() tea.Cmd {
 	return nil
 }
-
-// func (m *Model) numVisibleEntries() int {
-// 	i := m.entitiesTree.Children().Length()
-// 	for _, node := range m.entitiesTree.children.children {
-// 		if node.isExpanded {
-// 			i += node.Children().Length()
-// 		}
-// 	}
-// 	return i
-// }
 
 func getIcon(kind string) string {
 	switch kind {
@@ -104,52 +101,9 @@ func getIcon(kind string) string {
 	return ""
 }
 
-func buildShownEntries(tree *Node, level int) []entry {
-	lines := make([]entry, 0)
-	indent := strings.Repeat(" ", level*2)
-
-	sort.Slice(tree.children.children, func(i, j int) bool {
-		return tree.children.children[i].name < tree.children.children[j].name
-	})
-
-	for _, child := range tree.children.children {
-		text := indent + child.String()
-		lines = append(lines, entry{text: text, ref: child})
-		if child.isExpanded {
-			lines = append(lines, buildShownEntries(child, level+1)...)
-		}
-	}
-	return lines
-}
-
-func MyEnumerator(children tree.Children, index int) string {
-	if children.Length() == 0 {
-		return ""
-	}
-	a := children.At(0)
-	_ = a
-	// c := .(*Node)
-	// if c.kind == "Namespace" {
-	// 	return ""
-	// }
-	if children.Length()-1 == index {
-		return "╰──"
-	}
-	return "├──"
-}
-
-// func buildShownTree(m Model, orig *Node) *tree.Tree {
-// 	t := tree.New().Enumerator(MyEnumerator)
-
-// 	for _, child := range orig.children.children {
-// 		sub := tree.Root(child)
-// 		if child.children.Length() > 0 {
-// 			nodes := buildShownTree(m, child)
-// 			sub.Child(nodes)
-// 		}
-// 		t.Child(sub)
-// 	}
-// 	return t
+// 	if children.Length()-1 == index {
+// 		return "╰──"
+// 	return "├──"
 // }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
@@ -160,7 +114,7 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		m = m.rebuildEntries()
 		if m.cursor == -1 && len(m.entries) > 0 {
 			m.cursor = 0
-			cmd = selectEntity(m.entries[m.cursor].ref)
+			cmd = selectEntity(m.entries[m.cursor])
 		}
 	case tea.KeyMsg:
 		if m.focused {
@@ -169,27 +123,32 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 				if m.cursor > 0 {
 					m.cursor--
 				}
-				cmd = selectEntity(m.entries[m.cursor].ref)
+				cmd = selectEntity(m.entries[m.cursor])
 			case "down", "j":
 				if m.cursor < len(m.entries)-1 {
 					m.cursor++
 				}
-				cmd = selectEntity(m.entries[m.cursor].ref)
+				cmd = selectEntity(m.entries[m.cursor])
 			case " ":
-				m.entries[m.cursor].ref.isExpanded = !m.entries[m.cursor].ref.isExpanded
-				m.entries = buildShownEntries(m.entitiesTree, 0)
+				e := m.entries[m.cursor]
+				m.expandEntry(e, !e.isExpanded)
+				// idx := getEntriesIndex(m.entities, m.entries[m.cursor].entity.GetId())
+				// m.entities[m.cursor].isExpanded = !m.entries[m.cursor].isExpanded
+				// m.entries = buildShownEntries(m.entities)
 			case "right", "l":
-				m.entries[m.cursor].ref.isExpanded = true
-				m.entries = buildShownEntries(m.entitiesTree, 0)
+				m.expandEntry(m.entities[m.cursor], true)
+				// m.entries[m.cursor].isExpanded = true
+				// m.entries = buildShownEntries(m.entities)
 			case "left", "h":
-				m.entries[m.cursor].ref.isExpanded = false
-				m.entries = buildShownEntries(m.entitiesTree, 0)
+				m.expandEntry(m.entities[m.cursor], false)
+				// m.entries[m.cursor].isExpanded = false
+				// m.entries = buildShownEntries(m.entities)
 			case "g": // go to the top
 				m.cursor = 0
-				cmd = selectEntity(m.entries[m.cursor].ref)
+				cmd = selectEntity(m.entries[m.cursor])
 			case "G": // go to the bottom
 				m.cursor = len(m.entries) - 1
-				cmd = selectEntity(m.entries[m.cursor].ref)
+				cmd = selectEntity(m.entries[m.cursor])
 			case "enter":
 				// if m.cursor >= 0 {
 				// 	cmd = selectEntity(m.entries[m.cursor].ref)
@@ -204,29 +163,99 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	return m, cmd
 }
 
+func (m *Model) expandEntry(entry entry, isExpanded bool) {
+	idx := getEntriesIndex(m.entities, entry)
+	if idx >= 0 {
+		m.entities[idx].isExpanded = isExpanded
+		m.entries = buildShownEntries(m.entities)
+	}
+}
+
+func getEntriesIndex(entries []entry, entry entry) int {
+	entryID := entry.entity.GetId()
+	for i, e := range entries {
+		if e.entity.GetId() == entryID {
+			return i
+		}
+	}
+	return -1
+}
+
 func (m Model) rebuildEntries() Model {
 	// keep track of previously opened nodes
 	expandedNodes := make(map[string]struct{})
-	m.entitiesTree.traverse(func(n *Node) bool {
-		if n.isExpanded {
-			expandedNodes[n.id] = struct{}{}
+	for _, e := range m.entries {
+		expandedNodes[e.entity.GetId()] = struct{}{}
+	}
+	var checkExpandedFn func(entry) bool
+	if len(m.entries) == 0 {
+		checkExpandedFn = func(e entry) bool {
+			return e.level <= 1
 		}
-		return n.isExpanded
+	} else {
+		checkExpandedFn = func(e entry) bool {
+			_, isExpanded := expandedNodes[e.entity.GetId()]
+			return isExpanded
+		}
+	}
+
+	adj := m.campaign.GetGraph()
+	entities := m.campaign.GetEntities()
+
+	m.entities = make([]entry, 0)
+	depthFirstTraversal(adj, "cluster", func(n string, level int) {
+		if e, ok := entities[n]; ok {
+			entry := entry{
+				level:       level,
+				entity:      e,
+				numChildren: len(adj[e.GetId()]),
+			}
+			entry.isExpanded = checkExpandedFn(entry)
+			m.entities = append(m.entities, entry)
+		}
 	})
 
-	// start new tree from scratch
-	m.entitiesTree = newTree()
-	entities := m.campaign.GetEntities()
-	for _, entity := range entities {
-		addEntity(m.entitiesTree, m.campaign, entity, expandedNodes)
-	}
-	m.entries = buildShownEntries(m.entitiesTree, 0)
+	m.entries = buildShownEntries(m.entities)
 	return m
 }
 
-func selectEntity(node *Node) tea.Cmd {
+func depthFirstTraversal(adjList map[string]map[string]string, startNode string, visit func(node string, level int)) {
+	visited := make(map[string]bool)
+	var dfs func(node string, level int)
+	dfs = func(node string, level int) {
+		if visited[node] {
+			return
+		}
+		visited[node] = true
+		visit(node, level)
+		for neighbor := range adjList[node] {
+			dfs(neighbor, level+1)
+		}
+	}
+	dfs(startNode, 0)
+}
+
+func buildShownEntries(entries []entry) []entry {
+	lines := make([]entry, 0)
+
+	for i := 0; i < len(entries); i++ {
+		e := entries[i]
+		e.label = e.Label()
+		lines = append(lines, e)
+		if !e.isExpanded && e.numChildren > 0 {
+			// skip all children
+			skipLevel := e.level
+			for i+1 < len(entries) && entries[i+1].level > skipLevel {
+				i++
+			}
+		}
+	}
+	return lines
+}
+
+func selectEntity(e entry) tea.Cmd {
 	return func() tea.Msg {
-		return tuimsg.EntitySelected{Id: node.id, Kind: node.kind, Name: node.name, AccessLevel: node.accessLevel}
+		return tuimsg.EntitySelected{Id: e.entity.GetId(), Kind: e.entity.GetKind(), Name: e.entity.GetName(), AccessLevel: e.accessLevel}
 	}
 }
 
@@ -240,18 +269,18 @@ func (m Model) View() string {
 	var style lipgloss.Style
 	for _, entry := range m.entries {
 		style = lineStyle
-		if entry.ref.isPwnd {
+		if entry.isPwnd {
 			style = pwndStyle
 		}
 
-		lines = append(lines, style.Render(entry.text))
+		lines = append(lines, style.Render(entry.label))
 	}
 
 	selectedStyle := lipgloss.NewStyle().Bold(true).UnsetForeground().Background(theme.PrimaryColor)
 	if m.cursor >= 0 && len(lines) > 0 {
 		// use the raw text again to avoid conflicting styles
 		e := m.entries[m.cursor]
-		lines[m.cursor] = selectedStyle.Render(e.text)
+		lines[m.cursor] = selectedStyle.Render(e.label)
 	}
 	s += strings.Join(lines, "\n")
 
@@ -274,5 +303,5 @@ func (m Model) GetSelectedEntity() string {
 	if m.cursor < 0 {
 		return ""
 	}
-	return m.entries[m.cursor].ref.id
+	return m.entries[m.cursor].entity.GetId()
 }
