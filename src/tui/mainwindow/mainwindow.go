a768ec9738be889dced3a380f1ec3cddae126610
Markus Gierlinger <mataryki@gmail.com>
2025-02-13 23:04:28 +0100

    add option to show image in TUI

--- src/tui/mainwindow/mainwindow.go
+++ src/tui/mainwindow/mainwindow.go
@@ -28,7 +28,7 @@ type Model struct {
 	showEntityDetails bool
 }
 
-func NewMainWindow(width float32, bottomOffset int) Model {
+func NewMainWindow(c *campaign.Campaign, width float32, bottomOffset int) Model {
 	var style = lipgloss.NewStyle().
 		Bold(true).
 		// Foreground(lipgloss.Color("#7D56F4")).
@@ -70,6 +70,8 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		} else {
 			slog.Warn("TUI", "", "Failed to resolve "+msg.Id)
 		}
+	case domain.GraphRendered:
+		m.showEntityDetails = false
 	}
 
 	return m, nil
@@ -79,6 +81,10 @@ func (m Model) View() string {
 	var s string
 	if m.showEntityDetails {
 		s = renderEntity(m.entity)
+	} else {
+		if imgStr, ok := getTopologyImgContent("topo.png", m.style.GetWidth()-1, m.style.GetHeight()-1); ok {
+			s += imgStr
+		}
 	}
 
 	if m.focused {
@@ -108,4 +114,76 @@ func (m *Model) Focus() {
 }
 func (m *Model) Blur() {
 	m.focused = false
+}
+
+func getTopologyImgContent(path string, width, height int) (string, bool) {
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return "", false
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	img, err := png.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the expected size that you want:
+	// dst := image.NewRGBA(image.Rect(0, 0, img.Bounds().Max.X/2, img.Bounds().Max.Y/2))
+	// dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// Resize:
+	// draw.NearestNeighbor.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
+
+	var buf bytes.Buffer
+	err = Fprint(&buf, img, width, height)
+	if err != nil {
+		return "", false
+	} else {
+		a := buf.Bytes()[:]
+		return string(a), true
+	}
+}
+
+// Source: https://github.com/dolmen-go/kittyimg
+func Fprint(w io.Writer, img image.Image, cols int, rows int) error {
+	bounds := img.Bounds()
+
+	// transmitAndDisplay := "a=T"
+	// 32bit := f=32
+
+	// f=32 => RGBA
+	_, err := fmt.Fprintf(w, "\033_Gq=1,a=T,f=32,s=%d,v=%d,c=%d,t=d,", bounds.Dx(), bounds.Dy(), cols)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 0, 4*4096) // Multiple of 4 (RGBA)
+
+	// var p streamPayload
+	var p zlibPayload
+	p.Reset(w)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			if len(buf) == cap(buf) {
+				if _, err = p.Write(buf); err != nil {
+					return err
+				}
+				buf = buf[:0]
+			}
+			r, g, b, a := img.At(x, y).RGBA()
+			// A color's RGBA method returns values in the range [0, 65535].
+			// Shifting by 8 reduces this to the range [0, 255].
+			buf = append(buf, byte(r>>8), byte(g>>8), byte(b>>8), byte(a>>8))
+		}
+	}
+
+	if _, err = p.Write(buf); err != nil {
+		return err
+	}
+	return p.Close()
 }
