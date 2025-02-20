9ba527147b09f10fbb7ed1af913feed1a121377f
Markus Gierlinger <mataryki@gmail.com>
2025-02-20 20:00:00 +0100

    remove old python PoC code

--- c2/src/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Markus Gierlinger <markus.gierlinger@dynatrace.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- c2/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Markus Gierlinger <markus.gierlinger@dynatrace.com>
-#
-# SPDX-License-Identifier: MIT
