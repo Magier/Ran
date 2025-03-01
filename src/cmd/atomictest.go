6bbd42fcb38b39a1fa84db95c816d1fc9901fe50
Markus Gierlinger <mataryki@gmail.com>
2025-03-01 10:44:37 +0100

    allow setting a single target when starting a campaign

--- src/cmd/atomictest.go
+++ src/cmd/atomictest.go
@@ -7,15 +7,16 @@ import (
 
 func newAtomicTestCmd() *cobra.Command {
 	var godMode bool
+	var target string
 	cmd := &cobra.Command{
 		Use:   "test",
 		Short: "Run an atomic test in a Kubernetes cluster",
 		Run: func(cmd *cobra.Command, args []string) {
-			core.StartRan(true, godMode)
+			core.StartRan(true, godMode, target)
 		},
 	}
 
 	cmd.Flags().BoolVar(&godMode, "godmode", false, "enable Godmode to use the local kubeconfig context to load all available resources")
-	cmd.Flags().StringP("target", "t", "", `set the initial target for the emulation. In the pattern "<ns>/<service or pod>" or a URL`)
+	cmd.Flags().StringVarP(&target, "target", "t", "", `set the initial target for the emulation. In the pattern "<ns>/<service or pod>" or a URL`)
 	return cmd
 }
