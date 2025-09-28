3d292378c4dbefa933c0652be61f82de9c25cb67
Markus Gierlinger <mataryki@gmail.com>
2025-09-28 20:57:31 +0200

    improve UI esp. flow info

--- RanUI/frontend/src/lib/components/flow/attack_node.svelte
+++ RanUI/frontend/src/lib/components/flow/attack_node.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Handle, Position, type NodeProps } from '@xyflow/svelte';
 	import { campaign } from '$lib/wailsjs/go/models';
+	import { iconMap } from '$lib/tactic_icons';
 	import Icon from '@iconify/svelte';
 
 	interface ActionNodeData extends Record<string, unknown> {
@@ -20,16 +21,35 @@
 
 	const { label, step } = data;
 
-	const { statusBorder, icon, color } = data.step?.Success
+	const { statusBorder, icon: stateIcon, color } = data.step?.Success
 		? { statusBorder: 'border-green-500', icon: 'lucide:check', color: 'text-success-500' }
 		: { statusBorder: 'border-red-500', icon: 'lucide:x', color: 'text-error-500' };
 </script>
 
-<div class={['border-1 rounded-md border-solid p-2 pb-4', statusBorder]}>
-	<span class="">{label}</span>
+<div class={['bg-surface-50-950 border-1 rounded-md border-solid px-2 py-2', statusBorder]}>
+		<span class="text-base">{label}</span>
+		<pre class="text-xs">{step.Target.name}</pre>
+
+	<div class="w-full flex items-left mt-2">
+		<div class="flex">
+			<span class="badge bg-surface-100-900 text-tertiary-contrast-200-800">
+				<Icon icon={iconMap[step.TTP.tactic]} width="16"/>
+				{step.TTP.tactic}
+			</span>
+		</div>
+		<div class="flex-1"></div>
+		<div class="flex items-center space-x-1">
+			{#if step.Observables?.length > 0}
+				<Icon icon={"humbleicons:eye"} width="16" />
+			{/if}
+			{#if step?.Success}
+				<Icon class={color} icon={stateIcon} width="16" />
+			{/if}
+		</div>
+	</div>
+
 	<Handle type="target" position={targetPosition} {isConnectable} />
 	<Handle type="source" position={sourcePosition} {isConnectable} />
-	<Icon class={['absolute bottom-1 right-1', color].join(' ')} {icon} />
 </div>
 
 <style>
---
+++ RanUI/frontend/src/lib/components/observable_info.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { domain } from '$lib/wailsjs/go/models';
+
+	interface Observable {
+		title: string;
+		type: string
+	}
+    interface ObservableInfoProps {
+        observable: Observable;
+    }
+
+    let { observable }: ObservableInfoProps = $props();
+
+
+	function prettyPrint(obj: any): string {
+		if (typeof obj === 'string') {
+			return obj;
+			// } else if (typeof obj === 'object') {
+			// 	if (Array.isArray(obj)) {
+			// 		return obj.map((item) => prettyPrint(item)).join(', ');
+			// 	} else if (obj === null) {
+			// 		return 'null';
+			// 	} else {
+			// 		return JSON.stringify(obj, null, 2);
+			// 	}
+			// } else if (typeof obj === 'number') {
+			// 	return obj.toString();
+			// } else if (typeof obj === 'boolean') {
+			// 	return obj ? 'true' : 'false';
+		} else {
+			return JSON.stringify(obj, null, 2);
+		}
+	}
+</script>
+
+
+
+{#each Object.entries(observable || {}) as [label, data]}
+<div class="bg-surface-50-950 ">
+    <div><span class="font-bold mr-1">{label}:</span>{prettyPrint(data)}</div>
+</div>
+{/each}
