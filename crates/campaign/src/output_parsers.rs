13b5d3c1fa17f8d47ae0cfc80aa581bab871a180
Markus Gierlinger <ma.gierlinger@gmail.com>
2026-04-06 13:24:47 +0200

    improve handling if tool can't be written to pod

--- crates/campaign/src/output_parsers.rs
+++ crates/campaign/src/output_parsers.rs
@@ -1437,6 +1437,7 @@ mod tests {
             args: HashMap::new(),
             target_id: "ns/default/pod/demo".to_string(),
             exec_system_id: String::new(),
+            started_at_ms: 0,
         }
     }
 
